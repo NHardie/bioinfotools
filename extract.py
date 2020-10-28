be62413ebeae911c305bdf54537381d0824fdfd7
Ed Mothershaw <edward.mothershaw@clearscore.com>
2020-10-28 13:35:36 +0000

    order files

--- extract.py
+++ extract.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
         for cell in data['cells']:
             if cell['cell_type'] == 'code':
                 if not cell['source'][0].startswith('# Test code'):
-                    with open(f'extracted/block_{counter}.py', 'w') as out:
+
+                    with open(f'extracted/block_{counter if counter > 9 else "0" + str(counter)}.py', 'w') as out:
                         out.write("".join(cell['source']) + '\n')
                     counter += 1
